3712eef5c6d8b954ab10e6863bf3dafd6055a4a9
Olawuni Olagoke <33896026+olagokemills@users.noreply.github.com>
2017-11-25 15:08:26 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Home</title>
+        <link type="text/css" rel="stylesheet" href="bootstrap/css/bootstrap.min.css"/> 
+        <link type="text/css" rel="stylesheet" href="bootstrap/css/index.css"/>
+    </head>
+    <body>
+      <nav class="navbar navbar-default navari">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">E-learn</a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse navbar-right">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="index.php">Home</a></li>
+             <li><a href="portal/login.php">Student-Portal</a></li>
+			  <li><a href="portal/teacher_login.php">Teacher-Portal</a></li>
+              <li><a href="events.php">News</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">More <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+				 <li><a href="vacancy.php">Vacancies</a></li>
+                 <li><a href="facilities.php">Facilities</a></li>
+                  <li><a href="organogram.php">Organogram</a></li>
+				  <li><a href="faqs.php">FAQS</a></li>
+                  </ul>
+              </li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
+        <script src="bootstrap/js/jquery.js"></script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+        <!--<script src="bootstrap/js/index.js"></script>-->
+        
+        <!--<div id="mycarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <!--<ol class="carousel-indicators">
+    <li data-target="#mycarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#mycarousel" data-slide-to="1"></li>
+    <li data-target="#mycarousel" data-slide-to="2"></li>
+    <li data-target="#mycarousel" data-slide-to="3"></li>
+    <li data-target="#mycarousel" data-slide-to="4"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <!--<div class="carousel-inner" role="listbox">
+    <div class="item">
+        <img src="img/1.jpg" data-color="lightblue" alt="First Image">
+        <div class="carousel-caption">
+            <h3>First Image</h3>
+        </div>
+    </div>
+    <div class="item">
+        <img src="img/2.jpg" data-color="firebrick" alt="Second Image">
+        <div class="carousel-caption">
+            <h3>Second Image</h3>
+        </div>
+    </div>
+    <div class="item">
+        <img src="img/1.jpg" data-color="violet" alt="Third Image">
+        <div class="carousel-caption">
+            <h3>Third Image</h3>
+        </div>
+    </div>
+    <div class="item">
+        <img src="img/2.jpg" data-color="lightgreen" alt="Fourth Image">
+        <div class="carousel-caption">
+            <h3>Fourth Image</h3>
+        </div>
+    </div>
+    <div class="item">
+        <img src="img/2.jpg" data-color="tomato" alt="Fifth Image">
+        <div class="carousel-caption">
+            <h3>Fifth Image</h3>
+        </div>
+    </div>
+  </div>
+
+  <!-- Controls -->
+  <!--<a class="left carousel-control" href="#mycarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#mycarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>-->
+
+        
+        <div class="container">
+            <div class="row"></div>
+            <div class="row"></div>
+            <div class="row">
+                <div class="col-sm-12 first-bar">
+                    <p>
+                        Student reports, teacher portal, parents reports, exams portal, asignment submissions,
+                        live class...
+                        <br />
+                        E-learn caters to all your needs
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-2 col-md-offset-3">
+                    <span class="btn btn-new">Students</span>
+                </div>
+                <div class="col-md-2">
+                    <span class="btn btn-new">Teachers</span>
+                </div>  
+                <div class="col-md-2">
+                    <span class="btn btn-new">Parents</span>
+                </div>
+            </div>
+        </div>
+        
+       <footer class="footer">
+      <div class="container">
+        <p class="text-muted">Place sticky footer content here.</p>
+      </div>
+    </footer>
+  
+                    
+    </body>
+</html>
+
+<!--https://www.youtube.com/watch?v=-->
